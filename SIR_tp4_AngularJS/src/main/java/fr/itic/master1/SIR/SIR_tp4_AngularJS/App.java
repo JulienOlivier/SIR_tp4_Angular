a36731e711e7caa21a04bcb30837870f48895e7d
12002839 <12002839@e214m08.istic.univ-rennes1.fr>
2015-02-10 14:27:25 +0100

    Premier commit

---
+++ SIR_tp4_AngularJS/src/main/java/fr/itic/master1/SIR/SIR_tp4_AngularJS/App.java
@@ -0,0 +1,13 @@
+package fr.itic.master1.SIR.SIR_tp4_AngularJS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
